cb087004c0ef63f9deca891041d25ab0435ed620
Matej Buday <rplnt@o0.sk>
2014-07-13 18:43:58 +0200

    Separate file for db

---
+++ database.py
@@ -0,0 +1,19 @@
+import scraperwiki
+
+def create_db():
+    # people table
+    scraperwiki.sqlite.execute('''
+        CREATE TABLE IF NOT EXISTS people(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            email TEXT)
+    ''')
+
+    # categories table
+    scraperwiki.sqlite.execute('''
+        CREATE TABLE IF NOT EXISTS categories(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            parent_category INTEGER,
+            FOREIGN KEY (parent_category) REFERENCES categories(id))
+    ''')
